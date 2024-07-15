cd9b4f526fcb0478007badeb959046e6e63ea3a2
ZabdielpereZ <zabdielpm4@gmail.com>
2024-07-14 17:34:44 -0700

    first commit

---
+++ welcome.py
@@ -0,0 +1,18 @@
+# Hey this is cool coding stuff i did on sunday
+
+
+students = ["James", "Kareem", "Brian", "Zabdiel", "Pamauk"]
+
+for student in students:
+    print(student + " is going to crush next week!")
+
+
+
+    def birthday_generator(age):
+        new_age = age + 1
+        return f"you just had a birthday, you are now {new_age} years old"
+    print(birthday_generator(16))
+    print(birthday_generator(29))
+
+
+    
