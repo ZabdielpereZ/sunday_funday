6b40980eb345d73d7d89e69e7bf34a50f0824b16
ZabdielpereZ <zabdielpm4@gmail.com>
2024-07-14 18:47:07 -0700

    updates for travis

--- welcome.py
+++ welcome.py
@@ -15,4 +15,16 @@ for student in students:
     print(birthday_generator(29))
 
 
-    
+    #adding in more code so we can see the commit error message!
+
+    print("Hello I love to write out code its so fun!")
+
+def birthday_generator2(age):
+        new_age = age + 1
+        if new_age < 21:
+             return f"you just had a birthday, you are now {new_age}"
+        elif new_age > 21 and new_age <50:
+             return f"We wont tell anyone its your {new_age} birthday, your secret is safe!"
+print(birthday_generator(16))
+print(birthday_generator(29))
+print(birthday_generator(70))
